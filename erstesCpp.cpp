ef17ab03dd7fadbf52411f16fcba8745fb3cc046
Angeberman <219336380+Angeberman@users.noreply.github.com>
2025-11-20 14:27:48 +0000

    dritte Zeile

--- erstesCpp.cpp
+++ erstesCpp.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hello Sascha  Diesemal onlien Git" << endl;
+    cout << "Zeile zwei. WIr kommen naeher" << endl;
+       cout << "dritte Zeile" << endl;
     return 0;
 }
