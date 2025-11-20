4df02afe862fad59c12663600fe72fe3cf9bf169
Angeberman <219336380+Angeberman@users.noreply.github.com>
2025-11-20 14:35:20 +0000

    fuenf

--- erstesCpp.cpp
+++ erstesCpp.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Zeile zwei. WIr kommen naeher" << endl;
        cout << "dritte Zeile" << endl;
                 cout << "vierte Zeile offline" << endl;
+                cout << "fuenfte Zeile online" << endl;
     return 0;
 }
